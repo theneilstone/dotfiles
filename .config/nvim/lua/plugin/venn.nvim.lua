300a948f8e90f729ea72b7e5683a6d572c9d8e3d
theneilstone <the.neil.stone@outlook.com>
2025-10-24 06:03:45 +0800

    feat(nvim): add markdown plug

--- .config/nvim/lua/plugin/venn.nvim.lua
+++ .config/nvim/lua/plugin/venn.nvim.lua
@@ -8,19 +8,19 @@ return {
                 vim.b.venn_enabled = true
                 vim.cmd([[setlocal ve=all]])
                 -- draw a line on HJKL keystokes
-                vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
-                vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
-                vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
-                vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
-                -- draw a box by pressing "f" with visual selection
-                vim.api.nvim_buf_set_keymap(0, "v", "gF", ":VBox<CR>", { noremap = true })
+                vim.keymap.set("n", "J", "<C-v>j:VBox<CR>", { buffer = true, noremap = true })
+                vim.keymap.set("n", "K", "<C-v>k:VBox<CR>", { buffer = true, noremap = true })
+                vim.keymap.set("n", "L", "<C-v>l:VBox<CR>", { buffer = true, noremap = true })
+                vim.keymap.set("n", "H", "<C-v>h:VBox<CR>", { buffer = true, noremap = true })
+                -- draw a box by pressing "gF" with visual selection
+                vim.keymap.set("v", "gF", ":VBox<CR>", { buffer = true, noremap = true })
             else
                 vim.cmd([[setlocal ve=]])
-                vim.api.nvim_buf_del_keymap(0, "n", "J")
-                vim.api.nvim_buf_del_keymap(0, "n", "K")
-                vim.api.nvim_buf_del_keymap(0, "n", "L")
-                vim.api.nvim_buf_del_keymap(0, "n", "H")
-                vim.api.nvim_buf_del_keymap(0, "v", "gF")
+                pcall(vim.keymap.del, "n", "J", { buffer = true })
+                pcall(vim.keymap.del, "n", "K", { buffer = true })
+                pcall(vim.keymap.del, "n", "L", { buffer = true })
+                pcall(vim.keymap.del, "n", "H", { buffer = true })
+                pcall(vim.keymap.del, "v", "gF", { buffer = true })
                 vim.b.venn_enabled = nil
             end
         end
