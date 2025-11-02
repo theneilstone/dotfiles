9610cf6f8454a3a27152288572d57671673b2d46
theneilstone <the.neil.stone@outlook.com>
2025-11-03 06:54:54 +0800

    feat(nvim): modify bg color

--- .config/nvim/lua/plugin/tokyonight.nvim.lua
+++ .config/nvim/lua/plugin/tokyonight.nvim.lua
@@ -4,8 +4,8 @@ return {
         require("tokyonight").setup({
             style = "night",
             on_colors = function(colors)
-                colors.bg = "#0a0a12"
-                colors.bg_dark = "#05050a"
+                colors.bg = "#000000"
+                colors.bg_dark = "#000000"
                 colors.bg_float = "#0a0a12"
                 colors.bg_sidebar = "#0a0a12"
                 colors.bg_statusline = "#0a0a12"
